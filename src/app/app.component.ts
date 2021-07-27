a5d6b5812903bb256258c1d56a9d9aef9afffff6
abdelrahmansaf <cfglolz@gmail.com>
2021-07-27 16:18:52 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  title = 'Tour of hereos' ;
+  title = 'Tour Of Heroes' ;
 }
